67b2db50ab4eb6d81f30b58d735f752e59aaabf1
sccu <jujang@sk.com>
2012-04-12 21:07:15 +0900

    리팩토링

--- DtcArdbeg/src/net/skcomms/dtc/client/DtcArdbeg.java
+++ DtcArdbeg/src/net/skcomms/dtc/client/DtcArdbeg.java
@@ -34,33 +34,17 @@ public class DtcArdbeg implements EntryPoint {
 
   private static class Pair<K, V> {
     K key;
-
     V value;
 
-    /**
-     * @param name
-     * @param node
-     */
     public Pair(K key, V value) {
       this.key = key;
       this.value = value;
     }
   }
 
-  private final static String BASE_URL;
-  static {
-    int queryStringStart = Document.get().getURL().lastIndexOf('?');
-    int index;
-    if (queryStringStart == -1) {
-      index = Document.get().getURL().lastIndexOf('/');
-    }
-    else {
-      index = Document.get().getURL().lastIndexOf('/', queryStringStart);
-    }
-    BASE_URL = Document.get().getURL().substring(0, index + 1);
-  }
+  private final static String BASE_URL = DtcArdbeg.calculateBaseUrl();
 
-  private final static String DTC_HOME_URL = DtcArdbeg.BASE_URL + "_dtcproxy_/";
+  private final static String DTC_PROXY_URL = DtcArdbeg.BASE_URL + "_dtcproxy_/";
 
   private static ServiceDao serviceDao = new ServiceDao();
 
@@ -70,6 +54,7 @@ public class DtcArdbeg implements EntryPoint {
       $doc.styleSheets[0]
           .insertRule("div#dtcContainer iframe { background-position: 0px 0px; }", 0);
     }
+
     dtc = $doc.getElementsByTagName("iframe")[1];
     $doc.styleSheets[0].cssRules[0].style.backgroundPositionY = "-100px";
     dtc.contentWindow.onscroll = function() {
@@ -79,18 +64,26 @@ public class DtcArdbeg implements EntryPoint {
     };
   }-*/;
 
-  private static String createNewDtcUrl() {
-    String newUrl = "";
-
-    if (Window.Location.getParameter("b") != null) {
-      newUrl = DtcArdbeg.DTC_HOME_URL + "?b=" + Window.Location.getParameter("b");
-    } else if (Window.Location.getParameter("c") != null) {
-      newUrl = DtcArdbeg.DTC_HOME_URL + "?c=" + Window.Location.getParameter("c");
-    } else {
-      newUrl = DtcArdbeg.DTC_HOME_URL;
+  private static String calculateBaseUrl() {
+    int queryStringStart = Document.get().getURL().indexOf('?');
+    int index;
+    if (queryStringStart == -1) {
+      index = Document.get().getURL().lastIndexOf('/');
     }
+    else {
+      index = Document.get().getURL().lastIndexOf('/', queryStringStart);
+    }
+    return Document.get().getURL().substring(0, index + 1);
+  }
 
-    return newUrl;
+  private static String calculateInitialDtcUrl() {
+    if (Window.Location.getParameter("b") != null) {
+      return DtcArdbeg.DTC_PROXY_URL + "?b=" + Window.Location.getParameter("b");
+    } else if (Window.Location.getParameter("c") != null) {
+      return DtcArdbeg.DTC_PROXY_URL + "?c=" + Window.Location.getParameter("c");
+    } else {
+      return DtcArdbeg.DTC_PROXY_URL;
+    }
   }
 
   public static String getBaseUrl() {
@@ -100,8 +93,7 @@ public class DtcArdbeg implements EntryPoint {
   private static void sortServicesByVisitCount(List<Pair<Integer, Node>> rows) {
     Collections.sort(rows, new Comparator<Pair<Integer, Node>>() {
       @Override
-      public int compare(Pair<Integer, Node> arg0,
-          Pair<Integer, Node> arg1) {
+      public int compare(Pair<Integer, Node> arg0, Pair<Integer, Node> arg1) {
         return -arg0.key.compareTo(arg1.key);
       }
     });
@@ -109,12 +101,9 @@ public class DtcArdbeg implements EntryPoint {
 
   private final Frame dtcFrame = new Frame();
 
-  private final DtcNavigationBar navigationBar = new DtcNavigationBar(DtcArdbeg.DTC_HOME_URL);
+  private final DtcNavigationBar navigationBar = new DtcNavigationBar(DtcArdbeg.DTC_PROXY_URL);
 
-  /**
-   * @param doc
-   */
-  private void addCssIntoDtcFrame(Document doc) {
+  private void addCssLinkIntoDtcFrame(Document doc) {
     LinkElement link = doc.createLinkElement();
     link.setType("text/css");
     link.setAttribute("rel", "stylesheet");
@@ -122,12 +111,56 @@ public class DtcArdbeg implements EntryPoint {
     doc.getBody().appendChild(link);
   }
 
-  private void applyStylesToServiceRows(List<Pair<Integer, Node>> rows) {
-    for (Pair<Integer, Node> pair : rows) {
+  private void applyStylesToDtcDirectoryNodes(List<Pair<Integer, Node>> pairs) {
+    for (Pair<Integer, Node> pair : pairs) {
       if (pair.key == 0) {
         Element.as(pair.value).setAttribute("style", "color:gray; ");
       }
     }
+  }
+
+  private LoadHandler createDtcFrameLoadHandler() {
+    return new LoadHandler() {
+      @Override
+      public void onLoad(LoadEvent event) {
+        Document doc = IFrameElement.as(
+            DtcArdbeg.this.dtcFrame.getElement()).getContentDocument();
+
+        if (doc == null) {
+          return;
+        }
+
+        DtcArdbeg.addDtcFrameScrollEventHandler(DtcArdbeg.this);
+
+        DtcArdbeg.this.onLoadDtcFrame(doc);
+
+        if (doc.getURL().equals(DtcArdbeg.DTC_PROXY_URL)) {
+          DtcArdbeg.this.onLoadDtcHomePage(doc);
+        }
+
+        int index = doc.getURL().indexOf("?b=");
+        if (index != -1) {
+          String serviceName = doc.getURL().substring(index + 3)
+              .replaceAll("/", "");
+          DtcArdbeg.this.onLoadDtcServiceDirectoryPage(doc, serviceName);
+        }
+
+        index = doc.getURL().indexOf("?c=");
+        if (index != -1) {
+          DtcArdbeg.this.onLoadDtcTestPage();
+        }
+      }
+    };
+  }
+
+  private ResizeHandler createDtcFrameResizeHandler() {
+    return new ResizeHandler() {
+      @Override
+      public void onResize(ResizeEvent event) {
+        DtcArdbeg.this.dtcFrame.setPixelSize(Window.getClientWidth() - 30,
+            Window.getClientHeight() - 200);
+      }
+    };
   }
 
   private Element createSortedTableBody(Document doc, List<Pair<Integer, Node>> rows) {
@@ -203,52 +236,13 @@ public class DtcArdbeg implements EntryPoint {
   private void initializeDtcFrame() {
     this.dtcFrame.setPixelSize(Window.getClientWidth() - 30, Window.getClientHeight() - 135);
 
-    String dtcUrl = DtcArdbeg.createNewDtcUrl();
-    this.dtcFrame.setUrl(dtcUrl);
-    GWT.log("url => " + dtcUrl);
+    this.dtcFrame.addLoadHandler(this.createDtcFrameLoadHandler());
 
-    this.dtcFrame.addLoadHandler(new LoadHandler() {
-      @Override
-      public void onLoad(LoadEvent event) {
-        Document doc = IFrameElement.as(
-            DtcArdbeg.this.dtcFrame.getElement()).getContentDocument();
-
-        if (doc == null) {
-          return;
-        }
-
-        DtcArdbeg.addDtcFrameScrollEventHandler(DtcArdbeg.this);
-
-        DtcArdbeg.this.onLoadDtcFrame(doc);
-
-        if (doc.getURL().equals(DtcArdbeg.DTC_HOME_URL)) {
-          DtcArdbeg.this.onLoadDtcHomePage(doc);
-        }
-
-        int index = doc.getURL().indexOf("?b=");
-        if (index != -1) {
-          String serviceName = doc.getURL().substring(index + 3)
-              .replaceAll("/", "");
-          DtcArdbeg.this.onLoadDtcServiceDirectoryPage(doc, serviceName);
-        }
-
-        index = doc.getURL().indexOf("?c=");
-        if (index != -1) {
-          DtcArdbeg.this.onLoadDtcTestPage();
-        }
-      }
-
-    });
+    Window.addResizeHandler(this.createDtcFrameResizeHandler());
 
     RootPanel.get("dtcContainer").add(this.dtcFrame);
 
-    Window.addResizeHandler(new ResizeHandler() {
-      @Override
-      public void onResize(ResizeEvent event) {
-        DtcArdbeg.this.dtcFrame.setPixelSize(Window.getClientWidth() - 30,
-            Window.getClientHeight() - 200);
-      }
-    });
+    this.dtcFrame.setUrl(DtcArdbeg.calculateInitialDtcUrl());
   }
 
   /**
@@ -263,9 +257,9 @@ public class DtcArdbeg implements EntryPoint {
    * 
    */
   private void onLoadDtcHomePage(Document doc) {
-    this.addCssIntoDtcFrame(doc);
+    this.addCssLinkIntoDtcFrame(doc);
     this.removeComaparePageAnchor(doc);
-    this.sortServices();
+    this.sortDtcNodes();
   }
 
   /**
@@ -273,8 +267,8 @@ public class DtcArdbeg implements EntryPoint {
    * @param serviceName
    */
   private void onLoadDtcServiceDirectoryPage(Document doc, String serviceName) {
-    this.addCssIntoDtcFrame(doc);
-    if (doc.getReferrer().equals(DtcArdbeg.DTC_HOME_URL)) {
+    this.addCssLinkIntoDtcFrame(doc);
+    if (doc.getReferrer().equals(DtcArdbeg.DTC_PROXY_URL)) {
       DtcArdbeg.serviceDao.addVisitCount(serviceName);
     }
   }
@@ -285,8 +279,8 @@ public class DtcArdbeg implements EntryPoint {
 
   @Override
   public void onModuleLoad() {
-    this.initializeDtcFrame();
     this.navigationBar.initialize(this);
+    this.initializeDtcFrame();
   }
 
   private void onScrollDtcFrame() {
@@ -306,20 +300,12 @@ public class DtcArdbeg implements EntryPoint {
     doc.getBody().removeChild(br);
   }
 
-  /**
-   * @param href
-   */
   public void setDtcFrameUrl(String href) {
     this.dtcFrame.setUrl(href);
   }
 
-  /**
-	 * 
-	 */
-  void sortServices() {
-    String path = "/";
-
-    DtcService.Util.getInstance().getDir(path, new AsyncCallback<List<DtcNodeInfo>>() {
+  void sortDtcNodes() {
+    DtcService.Util.getInstance().getDir("/", new AsyncCallback<List<DtcNodeInfo>>() {
       @Override
       public void onFailure(Throwable caught) {
         caught.printStackTrace();
@@ -327,19 +313,18 @@ public class DtcArdbeg implements EntryPoint {
       }
 
       @Override
-      public void onSuccess(List<DtcNodeInfo> result) {
+      public void onSuccess(List<DtcNodeInfo> nodeInfos) {
         Document doc = IFrameElement.as(DtcArdbeg.this.dtcFrame.getElement()).getContentDocument();
         Element oldTableBody = doc.getElementsByTagName("tbody").getItem(0);
 
-        List<Pair<Integer, Node>> rows = DtcArdbeg.this.createTableRows(result);
+        List<Pair<Integer, Node>> rows = DtcArdbeg.this.createTableRows(nodeInfos);
         DtcArdbeg.sortServicesByVisitCount(rows);
-        DtcArdbeg.this.applyStylesToServiceRows(rows);
+        DtcArdbeg.this.applyStylesToDtcDirectoryNodes(rows);
         Element sortedBody = DtcArdbeg.this.createSortedTableBody(doc, rows);
 
         oldTableBody.getParentNode().replaceChild(sortedBody, oldTableBody);
       }
     });
-
   }
 
   private void updateNavigationBar(Document doc) {
