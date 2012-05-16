3b2301272ab06c0ab4554b326f74a2c122912a23
Minkyu Kang <pelcious@gmail.com>
2012-05-16 14:41:43 +0900

    #57: DtcUserConfigObserver 추가.

--- DtcArdbeg/src/net/skcomms/dtc/client/DtcConfigManager.java
+++ DtcArdbeg/src/net/skcomms/dtc/client/DtcConfigManager.java
@@ -1,5 +1,8 @@
 package net.skcomms.dtc.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.skcomms.dtc.shared.UserConfig;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -12,14 +15,29 @@ public class DtcConfigManager {
   }
 
   private UserConfig userConfig;
+  private final List<DtcUserConfigObserver> observers = new ArrayList<DtcUserConfigObserver>();
 
   private DtcConfigManager() {
+  }
+
+  public void addUserConfigObserver(DtcUserConfigObserver observer) {
+    observers.add(observer);
+  }
+
+  private void notifyObservers() {
+    for (DtcUserConfigObserver observer : observers) {
+      observer.onChangeUserConfig();
+    }
+  }
+
+  public void removeUserConfigObserver(DtcUserConfigObserver observer) {
+    observers.remove(observer);
   }
 
   public void setUsername(String userId) {
     if (userId == null || userId.isEmpty()) {
       System.out.println("Empty ID");
-      this.userConfig = UserConfig.EMPTY_CONFIG;
+      userConfig = UserConfig.EMPTY_CONFIG;
     } else {
       System.out.println("IDIDIDIDIDIDIDID");
       DtcUserConfigService.Util.getInstance().getUserConfig(userId,
@@ -27,13 +45,15 @@ public class DtcConfigManager {
             @Override
             public void onFailure(Throwable caught) {
               System.out.println(caught.toString());
-              DtcConfigManager.this.userConfig = UserConfig.EMPTY_CONFIG;
+              userConfig = UserConfig.EMPTY_CONFIG;
+              notifyObservers();
             }
 
             @Override
             public void onSuccess(UserConfig result) {
               System.out.println("UserConfig:" + result);
-              DtcConfigManager.this.userConfig = result;
+              userConfig = result;
+              notifyObservers();
             }
           });
     }
