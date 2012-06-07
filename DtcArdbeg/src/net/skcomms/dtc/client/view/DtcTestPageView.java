4f079f26006ae728595dfe5967dc222c778ea667
n2651 <n2651@P010D20515.SKComms.NET>
2012-06-07 17:22:12 +0900

    add NativeEventHandler

--- DtcArdbeg/src/net/skcomms/dtc/client/view/DtcTestPageView.java
+++ DtcArdbeg/src/net/skcomms/dtc/client/view/DtcTestPageView.java
@@ -13,9 +13,13 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.http.client.URL;
 import com.google.gwt.regexp.shared.MatchResult;
 import com.google.gwt.regexp.shared.RegExp;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Event.NativePreviewEvent;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.smartgwt.client.types.ListGridEditEvent;
 import com.smartgwt.client.types.Overflow;
+import com.smartgwt.client.util.BooleanCallback;
+import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.Button;
 import com.smartgwt.client.widgets.HTMLPane;
 import com.smartgwt.client.widgets.events.ClickEvent;
@@ -28,19 +32,17 @@ import com.smartgwt.client.widgets.grid.ListGridEditorContext;
 import com.smartgwt.client.widgets.grid.ListGridEditorCustomizer;
 import com.smartgwt.client.widgets.grid.ListGridField;
 import com.smartgwt.client.widgets.grid.ListGridRecord;
-import com.smartgwt.client.widgets.grid.events.EditCompleteEvent;
-import com.smartgwt.client.widgets.grid.events.EditCompleteHandler;
 import com.smartgwt.client.widgets.layout.HLayout;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 public class DtcTestPageView {
 
-  static class RequestFormRecord extends ListGridRecord {
+  static class RequestGridRecord extends ListGridRecord {
 
-    public RequestFormRecord() {
+    public RequestGridRecord() {
     }
 
-    public RequestFormRecord(int id, String key, String name, ComboBoxItem value) {
+    public RequestGridRecord(int id, String key, String name, ComboBoxItem value) {
 
       this.setId(id);
       this.setKey(key);
@@ -48,7 +50,7 @@ public class DtcTestPageView {
       this.setValue(value);
     }
 
-    public RequestFormRecord(int id, String key, String name, String value) {
+    public RequestGridRecord(int id, String key, String name, String value) {
 
       this.setId(id);
       this.setKey(key);
@@ -88,6 +90,7 @@ public class DtcTestPageView {
 
   }
 
+  private int invalidRecordIdx;
   private HLayout layout;
   private VLayout vLayoutLeft;
   private ListGrid requestFormGrid;
@@ -104,22 +107,22 @@ public class DtcTestPageView {
 
   private DtcTestPageViewObserver readyRequestDataCb;
 
-  private void createFormRecord() {
+  private void createGridRecord() {
 
     List<DtcRequestParameterModel> params = this.requestInfo.getParams();
-    List<RequestFormRecord> records = new ArrayList<RequestFormRecord>();
+    List<RequestGridRecord> records = new ArrayList<RequestGridRecord>();
 
     int index = 0;
     for (DtcRequestParameterModel param : params) {
       records
-          .add(new RequestFormRecord(index++, param.getKey(), param.getName(), param.getValue()));
+          .add(new RequestGridRecord(index++, param.getKey(), param.getName(), param.getValue()));
     }
 
     this.requestFormGrid.setData(records
-        .toArray(new RequestFormRecord[0]));
+        .toArray(new RequestGridRecord[0]));
 
     // add IP combo box
-    RequestFormRecord ipRrecord = new RequestFormRecord(params.size(), "IP", "ip_select",
+    RequestGridRecord ipRrecord = new RequestGridRecord(params.size(), "IP", "ip_select",
         this.requestInfo.getIpInfo().getIpText());
 
     this.requestFormGrid.addData(ipRrecord);
@@ -132,7 +135,7 @@ public class DtcTestPageView {
 
         if (field.getName().equals("value")) {
 
-          RequestFormRecord record = (RequestFormRecord) context.getEditedRecord();
+          RequestGridRecord record = (RequestGridRecord) context.getEditedRecord();
           if (record.getAttributeAsString("key").equals("IP")) {
 
             ComboBoxItem cbItem = new ComboBoxItem();
@@ -159,9 +162,6 @@ public class DtcTestPageView {
           } else {
 
             TextItem textItem = new TextItem();
-            textItem.setShowHint(true);
-            textItem.setShowHintInField(true);
-            textItem.setHint("Some Hint");
             return textItem;
           }
         }
@@ -215,26 +215,59 @@ public class DtcTestPageView {
     this.requestFormGrid.setCanEdit(true);
     this.requestFormGrid.setEditEvent(ListGridEditEvent.CLICK);
     this.requestFormGrid.setEditByCell(true);
-    this.requestFormGrid.addEditCompleteHandler(new EditCompleteHandler() {
-      @Override
-      public void onEditComplete(EditCompleteEvent event) {
-        DtcTestPageView.this.createRequestData();
-        DtcTestPageView.this.readyRequestDataCb.onReadyRequestData();
-      }
 
+    // this.requestFormGrid.addEditCompleteHandler(new EditCompleteHandler() {
+    // @Override
+    // public void onEditComplete(EditCompleteEvent event) {
+    // // TODO Auto-generated method stub
+    // if (DtcTestPageView.this.validateRequestData() == 0) {
+    // DtcTestPageView.this.createRequestData();
+    // DtcTestPageView.this.readyRequestDataCb.onReadyRequestData();
+    // }
+    // }
+    // });
+
+    Event.addNativePreviewHandler(new Event.NativePreviewHandler() {
+
+      @Override
+      public void onPreviewNativeEvent(NativePreviewEvent event) {
+        if (event.isFirstHandler()) {
+          // GWT.log("Event: " + Integer.toString(event.getTypeInt()));
+          if (event.getTypeInt() == Event.ONKEYUP) {
+
+            if (event.getNativeEvent().getKeyCode() == 13) {
+              if (DtcTestPageView.this.validateRequestData() == 0) {
+                DtcTestPageView.this.createRequestData();
+                DtcTestPageView.this.readyRequestDataCb.onReadyRequestData();
+              }
+            }
+          }
+        }
+      }
     });
+
     this.requestFormGrid.setHeight(1);
     this.requestFormGrid.setShowAllRecords(true);
     this.requestFormGrid.setBodyOverflow(Overflow.VISIBLE);
     this.requestFormGrid.setOverflow(Overflow.VISIBLE);
     this.requestFormGrid.setLeaveScrollbarGap(false);
+    this.requestFormGrid.setCanAutoFitFields(false);
+    this.requestFormGrid.setCanCollapseGroup(false);
 
-    this.createFormRecord();
+    this.createGridRecord();
 
     this.nameField = new ListGridField("key", "Name", 120);
     this.nameField.setCanEdit(false);
+    this.nameField.setCanFilter(false);
+    this.nameField.setCanSort(false);
+    this.nameField.setCanReorder(false);
+    this.nameField.setCanGroupBy(false);
 
     this.valueField = new ListGridField("value", "Value", 180);
+    this.valueField.setCanFilter(false);
+    this.valueField.setCanSort(false);
+    this.valueField.setCanReorder(false);
+    this.valueField.setCanGroupBy(false);
 
     this.requestFormGrid.setFields(this.nameField, this.valueField);
 
@@ -246,8 +279,10 @@ public class DtcTestPageView {
     this.searchButton.addClickHandler(new ClickHandler() {
       @Override
       public void onClick(ClickEvent event) {
-        DtcTestPageView.this.createRequestData();
-        DtcTestPageView.this.readyRequestDataCb.onReadyRequestData();
+        if (DtcTestPageView.this.validateRequestData() == 0) {
+          DtcTestPageView.this.createRequestData();
+          DtcTestPageView.this.readyRequestDataCb.onReadyRequestData();
+        }
       }
     });
 
@@ -321,5 +356,50 @@ public class DtcTestPageView {
 
   public void setRequestInfo(DtcRequestInfoModel requestInfo) {
     this.requestInfo = requestInfo;
+  }
+
+  public int validateRequestData() {
+
+    for (ListGridRecord record : this.requestFormGrid.getRecords()) {
+
+      // GWT.log("Key: " + record.getAttribute("key"));
+      if (record.getAttribute("key").toLowerCase().equals("query")) {
+
+        // GWT.log("Value: " + record.getAttribute("value"));
+
+        if (record.getAttribute("value") == null) {
+          this.invalidRecordIdx = this.requestFormGrid.getRecordIndex(record);
+          SC.warn("Invalid Query", new BooleanCallback() {
+
+            @Override
+            public void execute(Boolean value) {
+              int recordIdx = DtcTestPageView.this.invalidRecordIdx;
+              DtcTestPageView.this.requestFormGrid.selectRecord(recordIdx);
+            }
+
+          });
+          return -1;
+        }
+      } else if (record.getAttribute("name").toLowerCase().equals("ip_select")) {
+        RegExp regExp = RegExp.compile("[0-9]+.[0-9]+.[0-9]+.[0-9]+");
+        MatchResult match = regExp.exec(record.getAttribute("value"));
+        this.invalidRecordIdx = this.requestFormGrid.getRecordIndex(record);
+
+        if (match.getGroup(0) == null) {
+          SC.warn("Invalid IP value", new BooleanCallback() {
+
+            @Override
+            public void execute(Boolean value) {
+              int recordIdx = DtcTestPageView.this.invalidRecordIdx;
+              DtcTestPageView.this.requestFormGrid.selectRecord(recordIdx);
+            }
+
+          });
+          return -1;
+        }
+
+      }
+    }
+    return 0;
   }
 }
